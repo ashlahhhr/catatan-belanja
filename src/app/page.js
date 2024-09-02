434a715dbd6c9332ed0a8662da2f3481ae5df10c
ashlahhhr <ashlahhh.ra@gmail.com>
2024-09-03 02:07:15 +0700

    kayaknya selesai

--- src/app/page.js
+++ src/app/page.js
@@ -20,6 +20,7 @@ export default async function Page() {
   return (
     <div className=" space-y-2">
       <UangCreate />
+
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {data.map((uang) => {
           return (
